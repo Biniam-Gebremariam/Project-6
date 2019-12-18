754e1813539f93470409b62b1d7c26f9c7912347
Biniam-Gebremariam <56429979+Biniam-Gebremariam@users.noreply.github.com>
2019-12-18 13:38:46 +0300

    Update Bubble Sort.c

--- Bubble Sort.c
+++ Bubble Sort.c
@@ -7,7 +7,6 @@ void swap(int *xp, int *yp)
     *yp = temp; 
 } 
   
-// An optimized version of Bubble Sort 
 void bubbleSort(int arr[], int n) 
 { 
    int i, j; 
@@ -23,14 +22,12 @@ void bubbleSort(int arr[], int n)
            swapped = true; 
         } 
      } 
-  
-     // IF no two elements were swapped by inner loop, then break 
+   
      if (swapped == false) 
         break; 
    } 
 } 
-  
-/* Function to print an array */
+
 void printArray(int arr[], int size) 
 { 
     int i; 
@@ -39,7 +36,6 @@ void printArray(int arr[], int size)
     printf("n"); 
 } 
   
-// Driver program to test above functions 
 int main() 
 { 
     int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
